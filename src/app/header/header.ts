b5c39305976cd13041b768f5839af15007a2e31c
chiranjib22 <chiranjib.chakraborty.cse@gmail.com>
2026-05-16 03:52:32 +0600

    header template design complete

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'recipe-app-header',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
