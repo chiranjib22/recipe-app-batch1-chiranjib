0fb8a97f9823b8d35a439258276f25bf1ba46f51
chiranjib22 <chiranjib.chakraborty.cse@gmail.com>
2026-05-16 03:09:53 +0600

    all necessary components are created

---
+++ src/app/recipe-details/recipe-details.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'recipe-app-recipe-details',
+  imports: [],
+  templateUrl: './recipe-details.html',
+  styleUrl: './recipe-details.scss',
+})
+export class RecipeDetails {}
